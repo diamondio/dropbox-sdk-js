8bb6723c359e2229dda0e4d092b1ca80ff16a11f
Leander (Macbook) <me@leander.ca>
2016-12-16 14:29:09 -0800

    Using stream from server to download

--- src/download-request.js
+++ src/download-request.js
@@ -34,63 +34,20 @@ downloadRequest = function (path, args, auth, host, accessToken, selectUser) {
     throw new Error('Unexpected auth type: ' + auth);
   }
 
-  var promiseFunction = function (resolve, reject) {
-    var apiRequest;
-
-    function success(data) {
-      if (resolve) {
-        resolve(data);
+  var apiRequest = request.post(getBaseURL(host) + path)
+    .set('Authorization', 'Bearer ' + accessToken)
+    .set('Dropbox-API-Arg', JSON.stringify(args))
+    .on('request', function () {
+      if (this.xhr) {
+        this.xhr.responseType = 'blob';
       }
-    }
+    });
 
-    function failure(error) {
-      if (reject) {
-        reject(error);
-      }
-    }
+  if (selectUser) {
+    apiRequest = apiRequest.set('Dropbox-API-Select-User', selectUser);
+  }
 
-    function responseHandler(error, response) {
-      var data;
-      if (error) {
-        failure(buildCustomError(error, response));
-      } else {
-        // In the browser, the file is passed as a blob and in node the file is
-        // passed as a string of binary data.
-        data = JSON.parse(response.headers['dropbox-api-result']);
-        if (response.xhr) {
-          data.fileBlob = response.xhr.response;
-        } else {
-          data.fileBinary = response.res.text;
-        }
-        success(data);
-      }
-    }
-
-    apiRequest = request.post(getBaseURL(host) + path)
-      .set('Authorization', 'Bearer ' + accessToken)
-      .set('Dropbox-API-Arg', JSON.stringify(args))
-      .on('request', function () {
-        if (this.xhr) {
-          this.xhr.responseType = 'blob';
-        }
-      });
-
-    if (selectUser) {
-      apiRequest = apiRequest.set('Dropbox-API-Select-User', selectUser);
-    }
-
-    // Apply the node binary parser to the response if executing in node
-    if (typeof window === 'undefined') {
-      apiRequest
-        .buffer(true)
-        .parse(nodeBinaryParser)
-        .end(responseHandler);
-    } else {
-      apiRequest.end(responseHandler);
-    }
-  };
-
-  return new Promise(promiseFunction);
+  return apiRequest;
 };
 
 module.exports = downloadRequest;
